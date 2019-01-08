3b745aa38c67b4a3b15db58e41ff0d1ba2c869ba
DrSpecial <46390404+DrSpecial@users.noreply.github.com>
2019-01-08 12:03:10 +0800

    add quickSort functions to Mysorts.java
    add both quickSortList() and quickSortCustData() methods

--- custData/src/sortExample/MySorts.java
+++ custData/src/sortExample/MySorts.java
@@ -145,11 +145,128 @@ public class MySorts {
 		}     
 		return true;
 	}
-	public boolean quickSortList(ArrayList<String> al)
+	/*****************************************************************
+	 * 
+	 * @param al 	List to be sorted
+	 * @param first	begin index of the element
+	 * @param last	end index of the element
+	 * 
+	 * This method provides a quicksort on the list data.
+	 */
+	public static void quickSortList(List<String> al, int first, int last)
 	{
-		//copy, paste and modify!
-		return true;
+		if (al == null || al.size() == 0)
+			return;
+
+		if (first >= last)
+			return;
+
+		// pick the pivot(middle index element)
+		int middle = first + (last - first) / 2;
+		String pivot = al.get(middle);
+
+		// make left < pivot and right > pivot
+		int i = first, j = last;
+		while (i <= j) 
+		{
+		 	/**
+             		* In each iteration, we will identify a number from left side which 
+             		* is greater than the pivot value, and also we will identify a number 
+             		* from right side which is less then the pivot value. Once the search 
+             		* is done, we exchange both numbers.
+             		*/
+			while (al.get(i).compareToIgnoreCase(pivot) < 0)
+			{
+				i++;
+			}
+
+			while (al.get(j).compareToIgnoreCase(pivot) > 0)
+			{
+				j--;
+			}
+
+			if (i <= j) 
+			{
+				String temp = al.get(i);
+				al.set(i, al.get(j));
+				al.set(j, temp);
+				//move index to next position on both sides
+				i++;
+				j--;
+			}
+			
+		}		
+
+		// recursively sort two sub parts
+		if (first < j)
+			quickSortList(al, first, j);
+
+		if (last > i)
+			quickSortList(al, i, last);
 	}
+	
+	/********************************************************************
+	 * 
+	 * @param fileData 	List of CustData objects to be sorted.
+	 * @param first	the beginning index of the object
+	 * @param last	the ending index of the object
+	 * 
+	 * This method provides a first-level quick sort on Customer data 
+	 * by state. Upon completion, array list will be sorted by state,
+	 * then by zip.
+	 */
+	public static void quickSortCustData(List<sortExample.CustData> fileData, int first, int last)
+	{
+		if (fileData == null || fileData.size() == 0)
+			return;
+
+		if (first >= last)
+			return;
+
+		// pick the pivot(middle index element)
+		int middle = first + (last - first) / 2;
+		String pivot = fileData.get(middle).state;
+
+		// make left < pivot and right > pivot
+		int i = first, j = last;
+		while (i <= j) 
+		{
+			/**
+             		* In each iteration, we will identify a number from left side which 
+            		* is greater than the pivot value, and also we will identify a number 
+             		* from right side which is less then the pivot value. Once the search 
+             		* is done, we exchange both numbers.
+             		*/
+			while (fileData.get(i).state.compareToIgnoreCase(pivot) < 0)
+			{
+				i++;
+			}
+
+			while (fileData.get(j).state.compareToIgnoreCase(pivot) > 0)
+			{
+				j--;
+			}
+
+			if (i <= j) 
+			{
+				sortExample.CustData temp = fileData.get(i);
+				fileData.set(i, fileData.get(j));
+				fileData.set(j, temp);
+				//move index to next position on both sides
+				i++;
+				j--;
+			}
+			
+		}		
+
+		// recursively sort two sub parts
+		if (first < j)
+			quickSortCustData(fileData, first, j);
+
+		if (last > i)
+			quickSortCustData(fileData, i, last);
+	}
+	
 	public boolean mergeSortList(ArrayList<String> al)
 	{
 		//copy, paste and modify!
